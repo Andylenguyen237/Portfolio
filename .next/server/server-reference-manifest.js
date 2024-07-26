500bca5baf5ef7ab644120b83b0b85f74bb48017
Andylenguyen237 <lenguyenlinhdan02@gmail.com>
2024-07-26 14:28:16 +1000

    new components

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"78iXZPfhJ94ijeMaOD9/PVEewaU1SDGOrrDshwdYgOk=\"\n}"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
 
 const config: Config = {
   content: [
@@ -13,8 +17,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        upDown: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
+      animation: {
+        upDown: 'upDown 2s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
