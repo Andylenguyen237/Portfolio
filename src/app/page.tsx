b1a91d5a3f899d982b9518510b9db9edd25cb462
Andylenguyen237 <lenguyenlinhdan02@gmail.com>
2024-07-26 15:46:50 +1000

    new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import SolarCanvas from "./SolarCanvas";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import BallCanvas from "./Ball";
 
 export default function Home() {
   useEffect(() => {
@@ -12,7 +13,7 @@ export default function Home() {
   });
 
   return (
-    <>
+    <div className="flex flex-col h-full">
       <div className="flex h-full flex-col items-center justify-between bg-gradient-to-r from-[#04081B] via-[#1B244D] to-[#04081B] overflow-hidden">
         <NavBar />
         <div className="flex w-screen h-screen flex-grow">
@@ -20,48 +21,32 @@ export default function Home() {
         </div>
       </div>
       <div
-        className="bg-cover bg-center h-screen"
-        style={{ backgroundImage: "url('/herobg.png')" }}
+        className="bg-cover bg-center h-screen w-full z-10 relative"
+        style={{ backgroundImage: "url('/12.jpg')" }}
         data-aos="fade-up"
       >
-        <div className="mt-32 text-6xl text-center text-white">About</div>
+        <div className="mt-32 text-4xl text-center text-white"></div>
+
+        <div className="flex flex-row flex-wrap justify-center gap-10">
+          <div className="w-28 h-28" key="Javascript">
+            <BallCanvas icon="javascript-1.png" />
+            <BallCanvas icon="C.png" />
+            <BallCanvas icon="java.jpg" />
+            <BallCanvas icon="python.jpg" />
+            <BallCanvas icon="html.png" />
+            <BallCanvas icon="css.png" />
+            <BallCanvas icon="typescript.svg" />
+          </div>
+        </div>
       </div>
 
-      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 flex w-full justify-center border-b border-white bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="w-full h-screen content-center" data-aos="fade-up">
+        <div className="grid grid-cols-2">
+          <img src="astronaut.png" className="animate-upDown"></img>
+          <p>Hi my name is Andy</p>
         </div>
-      </div> */}
+      </div>
 
-      {/* <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div> */}
       <div className="flex h-screen flex-col items-center justify-between bg-gradient-to-r from-[#04081B] via-[#1B244D] to-[#04081B] overflow-hidden">
         <div
           className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left"
@@ -130,6 +115,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
